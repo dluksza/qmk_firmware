544aac9d279f051753ecb7a0b2434b572f5c5688
Dariusz Luksza <dariusz@luksza.org>
2021-07-14 11:31:29 +0100

    Add previous and next tab key binding

--- keyboards/keyboardio/atreus/keymaps/dluksza/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/dluksza/keymap.c
@@ -9,6 +9,10 @@
 #define BRIDN KC_F14
 // Mac brightness down
 #define BRIUP KC_F15
+// Previous tab
+#define TPREV SGUI(KC_LBRC)
+// Next tab
+#define TNEXT SGUI(KC_RBRC)
 // ESC key, when tapped, _LW when hold
 #define ESC_LW LT(_LW, KC_ESC)
 
@@ -37,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
     KC_NO,   KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
   /*
-   * insert home   up    end  pgup       ||       up     F7    F8    F9   F10
-   *  del   left  down  right pgdn       ||      down    F4    F5    F6   F11
-   * briup volup  bridn reset      back  || forw         F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl  || alt  space       prtsc scroll pause
+   * insert home   up    end  pgup           ||          up     F7    F8    F9   F10
+   *  del   left  down  right pgdn           ||         down    F4    F5    F6   F11
+   * briup volup  bridn reset      back      || forw            F1    F2    F3   F12
+   *       voldn  super shift bksp prev_tab  || next_tab alt  space       prtsc scroll pause
    */
   [_LW] = LAYOUT( /* [> LOWER <] */
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     BRIDN,   KC_VOLU, BRIUP,   KC_NO,   RESET,   KC_WBAK, KC_WFWD, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS )
+    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, TPREV,   TNEXT,   KC_SPC,  KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS )
 };
