b09ed8d446df1dcdcf1e64d1eb0b7898e7fa65bf
Dariusz Luksza <dariusz@luksza.org>
2021-07-16 08:28:35 +0100

    Add modtap OPT + GUI on TAB key

--- keyboards/keyboardio/atreus/keymaps/dluksza/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/dluksza/keymap.c
@@ -15,6 +15,8 @@
 #define TNEXT SGUI(KC_RBRC)
 // ESC key, when tapped, _LW when hold
 #define ESC_LW LT(_LW, KC_ESC)
+// TAB when tapped, OPT + GUI when held
+#define MOD_TAB LAG_T(KC_TAB)
 
 enum layer_names {
     _QW,
@@ -27,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    ESC_LW, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT ),
+    ESC_LW, MOD_TAB,KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT ),
 
   /*
    *  !       @     up     {    }        ||     pgup    7     8     9    *
