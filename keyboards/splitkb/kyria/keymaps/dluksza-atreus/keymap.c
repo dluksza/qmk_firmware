bb7e41eaf38ceceb7da6c3a67b313f295b9f8132
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-07-23 09:07:14 +0100

    Add curly braces to the QWERYTY layer

--- keyboards/splitkb/kyria/keymaps/dluksza-atreus/keymap.c
+++ keyboards/splitkb/kyria/keymaps/dluksza-atreus/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  * ,--------------------------------------------.                               ,----------------------------------------------.
- * |  (  )  |   Q   |   W  |   E  |   R  |   T  |                               |   Y   |   U  |   I  |   O   |   P   |        |
+ * |  (  )  |   Q   |   W  |   E  |   R  |   T  |                               |   Y   |   U  |   I  |   O   |   P   |  { }   |
  * |--------+-------+------+------+------+------|                               |-------+------+------+-------+-------+--------|
  * |  `  ~  |A/LShft|   S  |   D  |   F  |   G  |                               |   H   |   J  |   K  |   L   |;/LShft|Ctrl/' "|
  * |--------+-------+------+------+------+------+--------------.  ,-------------+-------+------+------+-------+-------+--------|
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                         `-----------------------------------'  `-----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     PAREN  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y  ,   KC_U ,  KC_I ,   KC_O ,  KC_P  , _______,
+                       PAREN  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y  ,   KC_U ,  KC_I ,   KC_O ,  KC_P  , KC_LCBR,
      GRV_SFT, KC_A,   KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H  ,   KC_J ,  KC_K ,   KC_L ,SCLN_SFT,CTL_QUOT,
      KC_TAB , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_BSLS,     _______, KC_RBRC, KC_N  ,   KC_M ,KC_COMM, KC_DOT ,SLSH_NUM, KC_ENT ,
                                KC_LCTL, KC_LGUI, KC_BSPC, ESC_SYM,_______,      KC_DEL, KC_LCTL,SPC_NAV, KC_RALT, _______
@@ -162,8 +162,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const key_override_t right_parentacy = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_RPRN);
+const key_override_t curly_braces = ko_make_basic(MOD_MASK_SHIFT, KC_LCBR, KC_RCBR);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &right_parentacy,
+    &curly_braces,
     NULL, // Null terminate the array of overrides!
 };
