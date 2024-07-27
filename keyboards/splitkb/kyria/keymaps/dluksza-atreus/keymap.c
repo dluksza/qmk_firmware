301c5b918c48a65dbb5ebe88366a55bc1b420d36
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-07-27 18:45:07 +0100

    Add MOD_TAB for TAB key when tapped and OPT+GUI when hold

--- keyboards/splitkb/kyria/keymaps/dluksza-atreus/keymap.c
+++ keyboards/splitkb/kyria/keymaps/dluksza-atreus/keymap.c
@@ -43,6 +43,8 @@ enum layers {
 #define GRV_SFT LSFT_T(KC_GRV)
 // shift when hold, KC_SCLN when tapped
 #define SCLN_SFT LSFT_T(KC_SCLN)
+// OPT+GUI when hold, KC_TAB when tapped
+#define MOD_TAB LAG_T(KC_TAB)
 // "(" on press, ")" with SFT
 #define PAREN KC_LPRN
 // Browser back
@@ -72,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
                        PAREN  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y  ,   KC_U ,  KC_I ,   KC_O ,  KC_P  , KC_LCBR,
      GRV_SFT, KC_A,   KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H  ,   KC_J ,  KC_K ,   KC_L ,SCLN_SFT,CTL_QUOT,
-     KC_TAB , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_BSLS,     _______, KC_RBRC, KC_N  ,   KC_M ,KC_COMM, KC_DOT ,SLSH_NUM, KC_ENT ,
+     MOD_TAB, KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_BSLS,     _______, KC_RBRC, KC_N  ,   KC_M ,KC_COMM, KC_DOT ,SLSH_NUM, KC_ENT ,
                                KC_LCTL, KC_LGUI, KC_BSPC, ESC_SYM,_______,      KC_DEL, KC_LCTL,SPC_NAV, KC_RALT, _______
     ),
 
