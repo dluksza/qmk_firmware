5074976c084c3b2aea3bb7dda0b1f18666e450f5
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-09-23 23:09:34 +0100

    Update definition of `key_override`, fixes compilation error

--- keyboards/splitkb/kyria/keymaps/dluksza-atreus/keymap.c
+++ keyboards/splitkb/kyria/keymaps/dluksza-atreus/keymap.c
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const key_override_t right_parentacy = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_RPRN);
 const key_override_t curly_braces = ko_make_basic(MOD_MASK_SHIFT, KC_LCBR, KC_RCBR);
 
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
     &right_parentacy,
     &curly_braces,
     NULL, // Null terminate the array of overrides!
